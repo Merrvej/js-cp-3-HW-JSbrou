754372bf3154bf9c2cee8d4d2a15a25c0b4d462d
Anastasiia Pavliuk <mervej@yandex.ru>
2023-09-20 21:18:40 +0500

    draft1

---
+++ async-requests/poll/task.js
@@ -0,0 +1,23 @@
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll')
+xhr.send();
+
+xhr.addEventListener('readystatechange', (e) => {
+    let result = JSON.parse(xhr.responseText);
+    
+    const titleElement = document.getElementById('poll__title');
+    titleElement.innerText = result.data.title;
+
+    const answersElement = document.getElementById('poll__answers');
+
+    const answersArr = arrayFrom(result.data.answers);
+
+    answersArr.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer;
+        button.addEventListener('click', () => {
+            alert('Спасибо, ваш голос засчитан!')
+        });
+        answersElement.appendChild(button);
+    })
+})
