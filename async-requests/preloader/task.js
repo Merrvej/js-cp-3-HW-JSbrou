0a4100662abbb66ff5a8193f936dfa111ec1994d
Anastasiia Pavliuk <mervej@yandex.ru>
2023-09-20 19:39:30 +0500

    draft1 doesntwork

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,11 +1,12 @@
 const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses', true);
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
 
-xhr.onload = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
+xhr.onload = function(e) {
+    e.preventDefault();
+    if (xhr.readyState === xhr.DONE) {
         const data = JSON.parse(xhr.responseText);
-        const valute = data.responce.valute;
+        const valute = data.response.Valute;
         displayData(valute);
         hideLoader();
       }
@@ -13,29 +14,29 @@ xhr.onload = function() {
 
 xhr.send();
 
-function displayData(valute) {
-    const itemsContainer = document.getElementById('items');
+function displayData(object) {
+    const itemsContainer = document.getElementById('item');
 
-    const codeElement = document.createElement('div');
-    codeElement.classList.add('item__code');
-    codeElement.textContent = item.CharCode;
-
-    const valueElement = document.createElement('div');
-    valueElement.classList.add('item__value');
-    valueElement.textContent = item.Value;
-
-    const currencyElement = document.createElement('div');
-    currencyElement.classList.add('item__currency');
-    currencyElement.textContent = 'руб.';
-
-    itemsContainer.appendChild(codeElement);
-    itemsContainer.appendChild(valueElement);
-    itemsContainer.appendChild(currencyElement);
+    for(let key = 0; key < object.length; key++) {
+        const codeElement = document.createElement('div');
+        codeElement.classList.add('item__code');
+        codeElement.textContent = object.[key].CharCode;
+    
+        const valueElement = document.createElement('div');
+        valueElement.classList.add('item__value');
+        valueElement.textContent = object.[key].Value;
+    
+        const currencyElement = document.createElement('div');
+        currencyElement.classList.add('item__currency');
+        currencyElement.textContent = 'руб.';
+    
+        itemsContainer.appendChild(codeElement);
+        itemsContainer.appendChild(valueElement);
+        itemsContainer.appendChild(currencyElement); 
+    }
 }
 
 function hideLoader() {
     const loader = document.getElementById('loader');
     loader.classList.remove('loader_active');
 }
-
-const loader = document.getElementById('loader');
