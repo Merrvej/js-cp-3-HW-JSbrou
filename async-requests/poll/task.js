e97fd1a11fcbab893d078b92ddbef671eebc816c
Anastasiia Pavliuk <mervej@yandex.ru>
2023-09-21 12:39:10 +0500

    draft4 expected final

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -4,19 +4,21 @@ xhr.send();
 
 xhr.addEventListener('readystatechange', (e) => {
     e.preventDefault();
-    let result = JSON.parse(xhr.responseText);
+    if(xhr.readyState === xhr.DONE) {
+        let result = JSON.parse(xhr.responseText);
     
-    const titleElement = document.getElementById('poll__title');
-    titleElement.innerText = result.data.title;
+        const titleElement = document.getElementById('poll__title');
+        titleElement.innerText = result.data.title;
 
-    const answersElement = document.getElementById('poll__answers');
+        const answersElement = document.getElementById('poll__answers');
 
-    result.data.answers.forEach(answer => {
-        const button = document.createElement('button');
-        button.innerText = answer;
-        button.addEventListener('click', () => {
-            alert('Спасибо, ваш голос засчитан!')
-        });
-        answersElement.appendChild(button);
-    })
+        result.data.answers.forEach(answer => {
+            const button = document.createElement('button');
+            button.innerText = answer;
+            button.addEventListener('click', () => {
+                alert('Спасибо, ваш голос засчитан!')
+            });
+            answersElement.appendChild(button);
+        })
+    }
 })
