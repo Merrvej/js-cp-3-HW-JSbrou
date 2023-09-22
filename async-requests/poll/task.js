62b3244d3e41cdf9363ac844e6c3e9eadecc67f3
Anastasiia Pavliuk <mervej@yandex.ru>
2023-09-22 14:50:09 +0500

    final1

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,24 +1,24 @@
 let xhr = new XMLHttpRequest();
+xhr.responseType - 'json';
 xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll')
 xhr.send();
 
-xhr.addEventListener('readystatechange', (e) => {
+xhr.addEventListener('load', (e) => {
     e.preventDefault();
-    if(xhr.readyState === xhr.DONE) {
-        let result = JSON.parse(xhr.responseText);
+    let result = xhr.responseText;
     
-        const titleElement = document.getElementById('poll__title');
-        titleElement.innerText = result.data.title;
+    const titleElement = document.getElementById('poll__title');
+    titleElement.innerText = result.data.title;
 
-        const answersElement = document.getElementById('poll__answers');
+    const answersElement = document.getElementById('poll__answers');
 
-        result.data.answers.forEach(answer => {
-            const button = document.createElement('button');
-            button.innerText = answer;
-            button.addEventListener('click', () => {
-                alert('Спасибо, ваш голос засчитан!')
-            });
-            answersElement.appendChild(button);
-        })
-    }
+    result.data.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer;
+        button.addEventListener('click', () => {
+        alert('Спасибо, ваш голос засчитан!')
+        });
+        answersElement.appendChild(button);
+
+    })
 })
